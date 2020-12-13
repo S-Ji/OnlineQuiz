e8aeb306fa19b0a378cabc66f17f0bc219e2fb37
webfullstack99 <webfullstack99@gmail.com>
2020-12-14 03:17:26 +0700

    test date with timezone

--- app/src/main/java/com/example/onlinequiz/Common/Helper.java
+++ app/src/main/java/com/example/onlinequiz/Common/Helper.java
@@ -3,10 +3,12 @@ package com.example.onlinequiz.Common;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class Helper {
+
     public static String getCurrentISODateString() {
         TimeZone tz = TimeZone.getTimeZone("UTC");
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
@@ -18,8 +20,9 @@ public class Helper {
     public static Date convertISODateStringToDate(String string) {
         DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         try {
-            Date result1 = df1.parse(string);
-            return result1;
+            Date date = df1.parse(string);
+            date = addHoursToJavaUtilDate(date, 7);
+            return date;
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
@@ -34,5 +37,12 @@ public class Helper {
             return stringDate;
         }
         return null;
+    }
+
+    public static Date addHoursToJavaUtilDate(Date date, int hours) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        return calendar.getTime();
     }
 }
