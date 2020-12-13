1194927721212b5c493a0bc7d6a580bf0143afad
webfullstack99 <webfullstack99@gmail.com>
2020-12-13 14:25:53 +0700

    better

--- app/src/main/java/com/example/onlinequiz/Playing.java
+++ app/src/main/java/com/example/onlinequiz/Playing.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onlinequiz.Common.Commom;
+import com.example.onlinequiz.Model.Question;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -24,15 +25,17 @@ public class Playing extends AppCompatActivity implements View.OnClickListener {
     int progressValue = 0;
 
     CountDownTimer mCountDown;
-    int index=0, score=0, thisQuestion=0, totalQuestion, correctAnswer;
+    int index = 0, score = 0, thisQuestion = 0, totalQuestion, correctAnswer;
 
     FirebaseDatabase database;
     DatabaseReference questions;
 
     ProgressBar progressBar;
     ImageView question_image;
-    Button btnA,btnB,btnC,btnD;
-    TextView txtScore, txtQuestionNum,question_text;
+    Button btnA, btnB, btnC, btnD;
+    TextView txtScore, txtQuestionNum, question_text;
+    MediaPlayer correctAnswerMp3;
+    MediaPlayer wrongAnswerMp3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,105 +45,109 @@ public class Playing extends AppCompatActivity implements View.OnClickListener {
         questions = database.getReference("Questions");
         mapping();
         initEvents();
-
     }
 
-    private void initEvents(){
+    private void initMp3() {
+        correctAnswerMp3 = MediaPlayer.create(this, R.raw.correct);
+        wrongAnswerMp3 = MediaPlayer.create(this, R.raw.wrong);
+    }
+
+    private void initEvents() {
         btnA.setOnClickListener(this);
         btnB.setOnClickListener(this);
         btnC.setOnClickListener(this);
         btnD.setOnClickListener(this);
     }
 
+    // ON ANSWER SELECTED
     @Override
     public void onClick(View v) {
         mCountDown.cancel();
-        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.correct);
-        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.wrong);
+        initMp3();
 
-        if(index < totalQuestion){
-            Button clickedButton = (Button)v;
-            if(clickedButton.getText().equals(Commom.questionsList.get(index).getCorrectAnswer())){
-                mp2.pause();
-                mp1.start();
-                score+=10;
-                correctAnswer++;
-                showQuestion(++index);
-//                addToQuestionRight(index);
-            }
-            else {
-                mp1.pause();
-                mp2.start();
-                showQuestion(++index);
-//                addToQuestionWrong(index);
-            }
-            txtScore.setText(String.format("%d",score));
-        } else {
-            Intent intent = new Intent(this,Done.class);
-            Bundle dataSend = new Bundle();
-            dataSend.putInt("SCORE",score);
-            dataSend.putInt("TOTAL",totalQuestion);
-            dataSend.putInt("CORRECTED",correctAnswer);
-            intent.putExtras(dataSend);
-            startActivity(intent);
-            finish();
-        }
+        if (index < totalQuestion) {
+            Button clickedButton = (Button) v;
+            String userAnswer = clickedButton.getText().toString();
+            if (isCorrectAnswer(userAnswer)) onUserSelectCorrectAnswer();
+            else onUserSelectWrongAnswer();
+            //
 
+            showQuestion(++index);
+            displayScore();
+        } else onDone();
     }
 
+    private void onUserSelectCorrectAnswer() {
+        wrongAnswerMp3.pause();
+        correctAnswerMp3.start();
+        score += 10;
+        correctAnswer++;
+    }
+
+    private void onUserSelectWrongAnswer() {
+        correctAnswerMp3.pause();
+        wrongAnswerMp3.start();
+    }
+
+    private void displayScore() {
+        txtScore.setText(String.format("%d", score));
+    }
+
+    private boolean isCorrectAnswer(String answer) {
+        return (answer.trim().equals(getCurrentQuestion().getCorrectAnswer().trim()));
+    }
+
+
+    // SHOW QUESTION
     private void showQuestion(int index) {
-        if(index < totalQuestion){
+        if (index < totalQuestion) {
             thisQuestion++;
             displayQuestionNum();
             resetProgress();
             displayQuestion();
             displayAnswer();
             mCountDown.start();
-        }
-        else onDone();
+        } else onDone();
     }
 
-    private void displayQuestionNum(){
-        txtQuestionNum.setText(String.format("%d / %d",thisQuestion,totalQuestion));
+    private void displayQuestionNum() {
+        txtQuestionNum.setText(String.format("%d / %d", thisQuestion, totalQuestion));
     }
 
-    private void resetProgress(){
+    private void resetProgress() {
         progressBar.setProgress(0);
-        progressValue=0;
+        progressValue = 0;
     }
 
-    private void displayQuestion(){
-        if(Commom.questionsList.get(index).getIsImageQuestion().equals("true")){
+    private void displayQuestion() {
+        if (getCurrentQuestion().getIsImageQuestion().equals("true")) {
             //if question img
             Picasso.with(getBaseContext())
-                    .load(Commom.questionsList.get(index).getQuestion())
+                    .load(getCurrentQuestion().getQuestion())
                     .into(question_image);
             question_image.setVisibility(View.VISIBLE);
             question_text.setVisibility(View.INVISIBLE);
-        }
-        else {
+        } else {
             //if question text, set img visible
-            question_text.setText(Commom.questionsList.get(index).getQuestion());
+            question_text.setText(getCurrentQuestion().getQuestion());
             question_image.setVisibility(View.INVISIBLE);
             question_text.setVisibility(View.VISIBLE);
         }
     }
 
-    private void displayAnswer(){
-        btnA.setText(Commom.questionsList.get(index).getA());
-        btnB.setText(Commom.questionsList.get(index).getB());
-        btnC.setText(Commom.questionsList.get(index).getC());
-        btnD.setText(Commom.questionsList.get(index).getD());
+    private void displayAnswer() {
+        btnA.setText(getCurrentQuestion().getA());
+        btnB.setText(getCurrentQuestion().getB());
+        btnC.setText(getCurrentQuestion().getC());
+        btnD.setText(getCurrentQuestion().getD());
     }
 
-
-    private void onDone(){
-        onDone();
-        Intent intent = new Intent(this,Done.class);
+    private void onDone() {
+        Intent intent = new Intent(this, Done.class);
         Bundle dataSend = new Bundle();
-        dataSend.putInt("SCORE",score);
-        dataSend.putInt("TOTAL",totalQuestion);
-        dataSend.putInt("CORRECTED",correctAnswer);
+        dataSend.putInt("SCORE", score);
+        dataSend.putInt("TOTAL", totalQuestion);
+        dataSend.putInt("CORRECTED", correctAnswer);
         intent.putExtras(dataSend);
         startActivity(intent);
         finish();
@@ -150,7 +157,7 @@ public class Playing extends AppCompatActivity implements View.OnClickListener {
     protected void onPostResume() {
         super.onPostResume();
         totalQuestion = Commom.testQuestionQty;
-        mCountDown = new CountDownTimer(TIMEOUT,INTERVAL) {
+        mCountDown = new CountDownTimer(TIMEOUT, INTERVAL) {
             @Override
             public void onTick(long millisUntilFinished) {
                 progressBar.setProgress(progressValue);
@@ -166,16 +173,21 @@ public class Playing extends AppCompatActivity implements View.OnClickListener {
         showQuestion(index);
     }
 
-    private void mapping(){
-        txtScore = (TextView)findViewById(R.id.txtScore);
-        txtQuestionNum = (TextView)findViewById(R.id.txtTotalQuestion);
-        question_text = (TextView)findViewById(R.id.question_text);
-        question_image = (ImageView)findViewById(R.id.question_image);
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+    private void mapping() {
+        txtScore = (TextView) findViewById(R.id.txtScore);
+        txtQuestionNum = (TextView) findViewById(R.id.txtTotalQuestion);
+        question_text = (TextView) findViewById(R.id.question_text);
+        question_image = (ImageView) findViewById(R.id.question_image);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        btnA = (Button)findViewById(R.id.btnAnswerA);
-        btnB = (Button)findViewById(R.id.btnAnswerB);
-        btnC = (Button)findViewById(R.id.btnAnswerC);
-        btnD = (Button)findViewById(R.id.btnAnswerD);
+        btnA = (Button) findViewById(R.id.btnAnswerA);
+        btnB = (Button) findViewById(R.id.btnAnswerB);
+        btnC = (Button) findViewById(R.id.btnAnswerC);
+        btnD = (Button) findViewById(R.id.btnAnswerD);
+    }
+
+    // SUPPORTED METHODS
+    private Question getCurrentQuestion() {
+        return Commom.questionsList.get(index);
     }
 }
